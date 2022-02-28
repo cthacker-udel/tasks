5e0e64dbb51330a70b737b14c6e2c463f6735671
Cameron Thacker <cthacker@udel.edu>
2022-02-28 02:55:35 -0500

    [solved-first-branch] fixed formatting errors with linter

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Cameron Thacker
-            </p>
+            <p>Cameron Thacker - Hello World</p>
         </div>
     );
 }
